58fb507c0f89e57af7e37d2bdb1e3743605766f4
cklynch2 <cklynch2@gmail.com>
2016-07-17 12:37:45 -0400

    Key frames defined but animation not triggered properly by IBAction method for button.

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,14 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bonJovi: UIImageView!
+    var bonJoviHeight : NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        bonJoviHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+        bonJoviHeight?.active = true
+    }
+
+    /* Make Bon Jovi grow in the expandButtonTapped(_:) method. You'll need to build a keyframe animation that implements the "bounce" effect. It will have steps close to this:
+        1. Shrink slightly below the starting height
+        2. Grow slightly beyond the height of the screen
+        3. Shrink back down to the exact height of the screen
+    */
+    
+    func expandBonJovi() {
+        UIView.animateKeyframesWithDuration(0.5, delay: 0, options: .CalculationModeLinear, animations: {
+            
+            let minimumHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.45)
+            let originalHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+            let maximumHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.1)
+            let viewHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor)
+            
+            let expansionHeights = [originalHeight, minimumHeight, maximumHeight, viewHeight]
+            
+            for (index, constraint) in expansionHeights.enumerate() {
+                UIView.addKeyframeWithRelativeStartTime(Double(index)/Double(expansionHeights.count), relativeDuration: 1.0/Double(expansionHeights.count), animations: {
+                    self.bonJoviHeight?.active = false
+                    self.bonJoviHeight? = constraint
+                    self.bonJoviHeight?.active = true
+                    self.view.layoutIfNeeded()
+                })
+            }
+            
+        }) { (true) in
+            self.bonJoviHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor)
+            self.bonJoviHeight?.active = true
+        }
+    }
+    
+    /* Now make the expandButtonTapped(_:) method toggle between the full-height and normal sizes of the image view. You'll probably have to add a flag property so you know whether to expand or shrink. The shrink animation should be the exact opposite of the expansion one:
+     1. Grow slightly beyond the height of the screen
+     2. Shrink down to slightly below the final height of the image
+     3. Return to the exact starting height
+     */
+    
+    func shrinkBonJovi() {
+        UIView.animateKeyframesWithDuration(0.6, delay: 0, options: .CalculationModeLinear, animations: {
+            
+            let minimumHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.45)
+            let originalHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+            let maximumHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.1)
+            let viewHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor)
+            
+            let expansionHeights = [viewHeight, maximumHeight, minimumHeight, originalHeight]
+            
+            for (index, constraint) in expansionHeights.enumerate() {
+                UIView.addKeyframeWithRelativeStartTime(Double(index)/Double(expansionHeights.count), relativeDuration: 1.0/Double(expansionHeights.count), animations: {
+                    self.bonJoviHeight?.active = false
+                    self.bonJoviHeight? = constraint
+                    self.bonJoviHeight?.active = true
+                })
+            }
+            
+        }) { (true) in
+            self.bonJoviHeight = self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+            self.bonJoviHeight?.active = true
+        }
     }
     
     @IBAction func expandButtonTapped(sender: AnyObject) {
         
+        // Starting from original height, should expand:
+        if bonJoviHeight == self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5) {
+            print("We entered the if statement for expanding.")
+            expandBonJovi()
+        
+        // Starting from full view height, should shrink:
+        } else if bonJoviHeight == self.bonJovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor) {
+            print("We entered the if statement for shrinking.")
+            shrinkBonJovi()
+        }
     }
+    
 
+    
+    
 }
 
